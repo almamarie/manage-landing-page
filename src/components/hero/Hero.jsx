08a6d439d9e94451a3a67705883bf16f0365dd9d
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-03-22 17:29:57 +0000

    Finished with hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import illustrationImg from "../../resources/images/illustration-intro.svg";
+import Button from "../ui/button/Button";
+
+const Hero = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.text}>
+        <h1>Bring everyone together to build better products</h1>
+        <p>
+          Manage makes it simple for software teams to plan day-to-day tasks
+          while keeping the larger team goals in view.
+        </p>
+        <Button className={styles.btn}>Get Started</Button>
+      </div>
+      <img
+        className={styles.illustration}
+        src={illustrationImg}
+        alt="illustration"
+      />
+    </div>
+  );
+};
+
+export default Hero;
